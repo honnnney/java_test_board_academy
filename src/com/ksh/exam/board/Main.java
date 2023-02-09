cb04d85f499b6659c29b5a1b6b0991128f2d3cc4
honnnney <tpgjs1023@gmail.com>
2023-02-09 18:38:08 +0900

    toString 메서드를 오버라이드 하여, 객체를 쉽게 디버깅 하기

--- src/com/ksh/exam/board/Main.java
+++ src/com/ksh/exam/board/Main.java
@@ -46,4 +46,9 @@ class Article {
   int id;
   String title;
   String body;
+
+  @Override
+  public String toString() {
+    return String.format("{id : %d, title : \"%s\", body : \"%s\"}", id, title, body);
+  }
 }
