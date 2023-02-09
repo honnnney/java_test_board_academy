4c1c148431fce5f1ac702b34c72c6f09b493b6fa
honnnney <tpgjs1023@gmail.com>
2023-02-09 19:45:30 +0900

    C:/Program Files/Git/usr/article/list

--- src/com/ksh/exam/board/Main.java
+++ src/com/ksh/exam/board/Main.java
@@ -10,6 +10,11 @@ public class Main {
     int articleLastId = 0;
     Article lastArticle = null;
 
+    List<Article> articles = new ArrayList<>();
+    articles.add(new Article(1,"제목1","내용1"));
+    articles.add(new Article(2,"제목2","내용2"));
+    articles.add(new Article(3,"제목3","내용3"));
+
     System.out.println("== 게시판 v 0.1  ==");
     System.out.println("== 프로그램 시작  ==");
 
@@ -20,7 +25,16 @@ public class Main {
       if (cmd.equals("exit")) {
         break;
       }
+      else if (cmd.equals("/usr/article/list")) {
+        System.out.println("-게시물 리스트-");
+        System.out.println("---------------");
+        System.out.println("번호  /  제목");
+        System.out.println("---------------");
 
+        for (Article article : articles){
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+      }
       else if (cmd.equals("/usr/article/detail")) {
         if (lastArticle == null){
           System.out.println("게시물이 존재하지 않습니다.");
