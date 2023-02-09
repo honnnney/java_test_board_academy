41d20e6acc390fc7a88529896ad57fb2ffddfdb9
honnnney <tpgjs1023@gmail.com>
2023-02-09 19:44:09 +0900

    C:/Program Files/Git/usr/article/detail

--- src/com/ksh/exam/board/Main.java
+++ src/com/ksh/exam/board/Main.java
@@ -1,19 +1,40 @@
 package com.ksh.exam.board;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
+    Article lastArticle = null;
+
     System.out.println("== 게시판 v 0.1  ==");
     System.out.println("== 프로그램 시작  ==");
+
+
     while (true) {
       System.out.printf("명령) ");
       String cmd = sc.nextLine();
       if (cmd.equals("exit")) {
         break;
       }
+
+      else if (cmd.equals("/usr/article/detail")) {
+        if (lastArticle == null){
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article article = lastArticle;
+        System.out.println("-게시물 상세내용-");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.body);
+
+      }
+
       else if (cmd.equals("/usr/article/write")) {
         System.out.println("== 게시물 등록 ==");
         System.out.printf("제목 : ");
@@ -25,11 +46,13 @@ public class Main {
 
 
         Article article = new Article(id, title, body);
+        lastArticle = article;
 
         System.out.println("생성 된 게시물 객체 : " + article);
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
+
       else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
       }
@@ -44,7 +67,7 @@ class Article {
   String title;
   String body;
 
-  Article(int id, String title, String body){
+  public Article(int id, String title, String body){
     this.id = id;
     this.body = body;
     this.title = title;
